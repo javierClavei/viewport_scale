47bc346ea4b8c87ac5b351400073ac7304eee0ef
Guy Segal <girokincanada@gmail.com>
2014-08-25 17:47:35 -0700

    first commit

---
+++ src/android/Viewport Scale.java
@@ -0,0 +1,44 @@
+package viewport_scale;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class CDVViewport Scale extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("coolMethod")) {
+            String message = args.getString(0);
+            this.coolMethod(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
+
+public class ViewportScale extends DroidGap
+{       
+    super.onCreate(savedInstanceState);
+    super.init(); // Don't forget this, you'll get runtime error otherwise!
+
+    // The following does the trick:
+    super.appView.getSettings().setUseWideViewPort(true);
+    super.appView.getSettings().setLoadWithOverviewMode(true);
+
+    super.loadUrl("file:///android_asset/www/index.htm");
+}
---
+++ src/ios/Viewport Scale.m
@@ -0,0 +1,29 @@
+/********* CDVViewport Scale.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface CDVViewport Scale : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation CDVViewport Scale
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+	self.webView.scalesPageToFit = YES;
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
