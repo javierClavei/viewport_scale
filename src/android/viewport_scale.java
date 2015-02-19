db69d9e86c74fb5ac26332376e9d265a2e17ee45
yourGuy <girokincanada@gmail.com>
2015-02-19 11:35:00 -0800

    Update viewport_scale.java

--- src/android/viewport_scale.java
+++ src/android/viewport_scale.java
@@ -10,7 +10,7 @@ import org.json.JSONObject;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class CDVViewport Scale extends CordovaPlugin {
+public class CDVViewportScale extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
