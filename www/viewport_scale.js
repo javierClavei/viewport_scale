737a2250881de29fabbf6f7332cf33643b9c3a23
yourGuy <girokincanada@gmail.com>
2015-02-19 17:42:57 -0800

    Update viewport_scale.js

--- www/viewport_scale.js
+++ www/viewport_scale.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
-
+ exec(null, null, "ViewportScale", "coolMethod", [arg0]);
 exports.coolMethod = function(arg0, success, error) {
     exec(success, error, "ViewportScale", "coolMethod", [arg0]);
 };
